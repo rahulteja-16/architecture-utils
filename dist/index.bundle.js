d59cd1f5643292ea07befc5ede1b5f1a5ca8653c
Rahul Teja <rahulteja.a@hotmail.com>
2022-02-22 13:14:19 +0530

    Changes

---
+++ dist/index.bundle.js
@@ -0,0 +1,28 @@
+
+  /**
+   * @license
+   * author: 
+   * lk_architecture_utils.js v1.0.0
+   * Released under the ISC license.
+   */
+
+var lk_architecture_utils = (function () {
+    'use strict';
+
+    var trim = (value = '') => {
+        return value.trim();
+    };
+
+    var odd_even = (value) => {
+        return value % 2 == 0 ? true : false;
+    };
+
+    var index = {
+        trim: trim,
+        odd_even: odd_even,
+    };
+
+    return index;
+
+})();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguYnVuZGxlLmpzIiwic291cmNlcyI6WyIuLi9zcmMvdHJpbS50cyIsIi4uL3NyYy9vZGRfZXZlbi50cyIsIi4uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCAodmFsdWUgPSAnJyk6IHN0cmluZyA9PiB7XG4gICAgcmV0dXJuIHZhbHVlLnRyaW0oKTtcbiAgfSIsImV4cG9ydCBkZWZhdWx0ICh2YWx1ZTogbnVtYmVyKTogYm9vbGVhbiA9PiB7XG4gICAgcmV0dXJuIHZhbHVlICUyID09MCA/IHRydWUgOiBmYWxzZTtcbiAgfSIsImltcG9ydCB0cmltIGZyb20gJy4vdHJpbSc7XG5pbXBvcnQgb2RkX2V2ZW4gZnJvbSAnLi9vZGRfZXZlbic7XG5cbmV4cG9ydCBkZWZhdWx0IHtcbiAgdHJpbTogdHJpbSxcbiAgb2RkX2V2ZW46IG9kZF9ldmVuLFxufVxuIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7O0FBQUEsZUFBZSxDQUFDLEtBQUssR0FBRyxFQUFFO1FBQ3RCLE9BQU8sS0FBSyxDQUFDLElBQUksRUFBRSxDQUFDO0lBQ3RCLENBQUM7O0FDRkgsbUJBQWUsQ0FBQyxLQUFhO1FBQ3pCLE9BQU8sS0FBSyxHQUFFLENBQUMsSUFBRyxDQUFDLEdBQUcsSUFBSSxHQUFHLEtBQUssQ0FBQztJQUNyQyxDQUFDOztBQ0NILGdCQUFlO1FBQ2IsSUFBSSxFQUFFLElBQUk7UUFDVixRQUFRLEVBQUUsUUFBUTtLQUNuQjs7Ozs7Ozs7In0=
